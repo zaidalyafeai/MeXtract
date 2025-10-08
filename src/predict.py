1796e02db72b898773f41b73a3968fa329a8e500
zaidalyafeai <alyafey22@gamil.com>
2025-10-08 15:30:04 +0300

    add an exmaple from the demo

--- src/predict.py
+++ src/predict.py
@@ -1,24 +1,64 @@
 from search import get_metadata
 from rich import print
+from schema import TextSchema
+from type_classes import *
 
-def extract(text, model_name, schema_name, backend = "openrouter", max_model_len = 8192, max_output_len = 2084):
+def extract(text, model_name, schema_name = "ar", backend = "openrouter", max_model_len = 8192, max_output_len = 2084, schema = None):
     message, metadata, cost, error = get_metadata(
-        text, model_name, schema_name=schema_name, backend = backend, log = False, max_model_len = max_model_len, max_output_len = max_output_len
+        text, model_name, schema_name=schema_name, schema = schema, backend = backend, log = False, max_model_len = max_model_len, max_output_len = max_output_len
     )
     return metadata
 
 text = """
-Mextract 1.0 is a 3B model that can extract metadata from any text. The model has been evaluated on the MOLE
-benchmark and achieved impressive results. The arhitecture is based on Transformer model. The model is released
-under the Apache 2.0 license. The model can process up to 8192 tokens. The model was released in 2025 from KAUST.
+MeXtract: Light-Weight Metadata Extraction from Scientific Papers
+
+Zaid Alyafeai, Maged AlShaibani, Bernard Ghanem
+
+Introduction
+MeXtract is a 3.0 Billion parameter chat model based on fine-tuning the chat version of Qwen2.5 3B.
+
+Model Overview
+MeXtract is evaluated on the MOLE+ benchmark, which is an extension of MOLE with model schema.
+
+Language	Number of papers
+Arabic	21
+Russian	21
+French	21
+English	21
+Japanese	21
+Multilingual	21
+Model	21
+
+Development Context
+We release the models freely, along with the code on GitHub (https://github.com/IVUL-KAUST/MeXtract
+) and on Hugging Face through https://huggingface.co/collections/IVUL-KAUST/mextract-68e63fb946b06f5031d4e3ef
+.
+The models are released under the Apache-2.0 License.
+
+Conclusion
+MeXtract is created through instruction tuning and preference optimization.
+The model achieves state-of-the-art results on the MOLE+ benchmark.
+
+preprint arXiv:2402.03177 [cs.CL] 5 Feb 2025
 """
 
-metadata = extract(
-    text, "Qwen2.5-3B-Instruct-kimi-k2-sft-8192", schema_name="model", backend = "vllm"   
-)
-print(metadata)
+class ExampleSchema(TextSchema):
+    Paper_Title: Field(Str, 1, 5)
+    Authors: Field(List[Str], 1, 5)
+    Name : Field(Str, 1, 5)
+    Num_Parameters: Field(Float, 1, 999)
+    Unit: Field(Str, 1, 5, ['Million', 'Billion', 'Trillion'])
+    Type: Field(Str, 1, 1, ['Base', 'Chat', 'Code'])
+    Benchmarks: Field(List[Str], 1, 5)
+    Access: Field(Str, 1, 1, ['Free', 'Paid'])
+    Host: Field(Str, 1, 5)
+    Link: Field(URL, 1, 1)
+    HF_Link: Field(URL, 1, 1)
+    License: Field(Str, 1, 5)
+    Paper_Link: Field(URL, 1, 1)
+    
 
 metadata = extract(
-    text, "moonshotai/kimi-k2", schema_name="model", backend = "openrouter"   
+    text, "IVUL-KAUST/MeXtract-3B", schema=ExampleSchema, backend = "transformers"   
 )
 print(metadata)
